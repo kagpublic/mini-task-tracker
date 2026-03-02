6cbf004f80f9761ad8ce02effb6dd35315800107
akuznetsov <kuznecovag@vtb.ru>
2026-03-02 11:20:48 +0700

    Initial commit.

---
+++ src/main/java/com/kagpublic/minitasktracker/MiniTaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.kagpublic.minitasktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTaskTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniTaskTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kagpublic/minitasktracker/MiniTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kagpublic.minitasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTaskTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
